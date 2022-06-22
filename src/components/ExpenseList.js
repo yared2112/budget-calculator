c76ceb4aed56c7427a394783bfd312cc56352c92
YaredT21 <teklewold.yared21@gmail.com>
2022-06-22 11:51:34 +0300

    completed task

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import ExpenseItem from './ExpenseItem'
+import {AiFillDelete} from "react-icons/ai"
 
-const ExpenseList = () => {
+const ExpenseList = ({expenses, handleDelete, handleEdit, clearItems}) => {
   return (
-    <div>ExpenseList</div>
+    <>
+        <ul className="list">
+            {expenses.map(expense => {
+                return (
+                    <ExpenseItem
+                        key={expense.id}
+                        expense={expense}
+                        handleDelete={handleDelete}
+                        handleEdit={handleEdit}
+                    />
+                );
+            })}
+        </ul>
+        {expenses.length > 0 && (
+        <button className="btn" onClick={clearItems}>
+            clear expenses
+            <AiFillDelete className="btn-icon" />
+        </button>
+        )}
+    </>
   )
 }
 
