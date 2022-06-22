cee210a085c8ba427c9e6c1ba33fc8035e9913e8
YaredT21 <teklewold.yared21@gmail.com>
2022-06-22 12:04:14 +0300

    completed task

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import {AiFillEdit, AiFillDelete} from "react-icons/ai"
 
-const ExpenseItem = () => {
-  return (
-    <div>ExpenseItem</div>
-  )
+const ExpenseItem = ({ expense }) => {
+    const { id, charge, amount } = expense;
+    return (
+        <li className="item">
+            <div className="info">
+                <span className="expense">{charge}</span>
+                <span className="amount">{amount}ETB</span>
+            </div>
+            <div>
+                <button
+                    className="edit-btn"
+                    aria-label="edit button"
+                    onClick={() => handleEdit(id)}
+                >
+                    <AiFillEdit />
+                </button>
+                <button
+                    className="clear-btn"
+                    aria-label="delete button"
+                    onClick={() => handleDelete(id)}
+                >
+                    <AiFillDelete />
+                </button>
+            </div>
+        </li>
+    )
 }
 
 export default ExpenseItem
