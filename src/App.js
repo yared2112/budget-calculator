aec1beefabdeaec25b288f8424e5ca55c7bc94b5
YaredT21 <teklewold.yared21@gmail.com>
2022-06-22 11:36:38 +0300

    completed task

--- src/App.js
+++ src/App.js
@@ -1,17 +1,133 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
 import ExpenseForm from './components/ExpenseForm';
-import ExpenseItem from './components/ExpenseItem';
 import ExpenseList from './components/ExpenseList';
 import Alert from './components/Alert';
+import { v4 as uuidv4 } from 'uuid';
+
+const initialExpenses = [
+  {id: uuidv4(), charge: "rent", amount: 5000},
+  {id: uuidv4(), charge: "car parking payment", amount: 500},
+  {id: uuidv4(), charge: "supplies", amount: 20000}
+];
 
 function App() {
+  // all expenses, add expense
+  const [expenses, setExpenses] = useState(initialExpenses);
+  // single expense
+  const [charge, setCharge] = useState("");
+  // single amount
+  const [amount, setAmount] = useState("");
+  // alert
+  const [alert, setAlert] = useState({ show: false });
+  // edit
+  const [edit, setEdit] = useState(false);
+  // id
+  const [id, setId] = useState(0)
+  
+  useEffect(() => {
+    console.log("called");
+
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+  // *********** functionality **************
+  //add charge
+  const handleCharge = e => {
+    setCharge(e.target.value);
+  };
+  // add amount
+  const handleAmount = e => {
+    let amount = e.target.value;
+    if (amount === "") {
+      setAmount(amount);
+    } else {
+      setAmount(parseInt(amount));
+    }
+  };
+
+  // handle alert
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 7000);
+  };
+  // handle submit
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      if (edit) {
+        let tempExpenses = expenses.map(item => {
+          return item.id === id ? { ...item, charge, amount } : item;
+        });
+        setExpenses(tempExpenses);
+        setEdit(false);
+      } else {
+        const singleExpense = { id: uuidv4(), charge, amount };
+        setExpenses([...expenses, singleExpense]);
+        handleAlert({ type: "success", text: "item added" });
+      }
+      // set charge back to empty string
+      setCharge("");
+      // set amount back to zero
+      setAmount("");
+    } else {
+      handleAlert({
+        type: "danger",
+        text: `charge can't be empty value and amount value has to be bigger than zero`
+      });
+    }
+  };
+  // handle delete
+  const handleDelete = id => {
+    let tempExpenses = expenses.filter(item => item.id !== id);
+    setExpenses(tempExpenses);
+    handleAlert({ type: "danger", text: "item deleted" });
+  };
+  //clear all items
+  const clearItems = () => {
+    setExpenses([]);
+  };
+  // handle edit
+  const handleEdit = id => {
+    let expense = expenses.find(item => item.id === id);
+    let { charge, amount } = expense;
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+    setId(id);
+  };
+
   return (
-    <div className="App">Budget Calculator
-    <ExpenseForm />
-    <ExpenseItem />
-    <ExpenseList />
-    <Alert />
-    </div>
+    <>
+      <Alert />
+      <h1>Budget Calculator</h1>
+      <main className="App">
+        <ExpenseForm 
+          handleSubmit={handleSubmit}
+          charge={charge}
+          handleCharge={handleCharge}
+          amount={amount}
+          handleAmount={handleAmount}
+          edit={edit}
+        />
+        <ExpenseList 
+          expenses={expenses}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          clearItems={clearItems}
+        />
+      </main>
+      <h1>
+        Total Spending :
+        <span className="total">
+          
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}ETB
+        </span>
+      </h1>
+    </>
     
   );
 }
