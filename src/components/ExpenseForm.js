1c19c05a537b16a4b5a25cacf7744d15c24d53ab
YaredT21 <teklewold.yared21@gmail.com>
2022-07-05 05:31:22 +0300

    completed task

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import {MdSend} from "react-icons/md"
 
-const ExpenseForm = (
-    charge,
-    amount,
-    handleCharge,
-    handleAmount,
-    handleSubmit,
-    edit
-) => {
+const ExpenseForm = () => {
   return (
-      <form onSubmit={handleSubmit}>
-        <div className="form-center">
-            <div className="form-group">
+    <form>
+        <div className='form-center'>
+            <div className='form-group'>
                 <label htmlFor="expense">charge</label>
                 <input
                     type="text"
@@ -20,28 +13,12 @@ const ExpenseForm = (
                     id="charge"
                     name="charge"
                     placeholder="e.g. rent"
-                    value={charge}
-                    onChange={handleCharge}
+                    //value={charge}
+                    //onChange={handleCharge}
                 />
             </div>
-            <div className="form-group">
-                <label htmlFor="amount">amount</label>
-                <input
-                    type="number"
-                    className="form-control"
-                    id="amount"
-                    name="amount"
-                    placeholder="e.g. 100"
-                    value={amount}
-                    onChange={handleAmount}
-                />
-            </div>
-      </div>
-      <button type="submit" className="btn">
-        {edit ? "edit" : "submit"}
-        {/* submit  */}
-        <MdSend className="btn-icon" />
-      </button>
+        
+        </div>
     </form>
   )
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,34 +1,32 @@
 import React from 'react'
 import {AiFillEdit, AiFillDelete} from "react-icons/ai"
-const ExpenseItem = ({ 
-    expense: { id, charge, amount },
-    handleDelete,
-    handleEdit 
-}) => {
-    return (
-        <li className="item">
-            <div className="info">
-                <span className="expense">{charge}</span>
-                <span className="amount">{amount}ETB</span>
-            </div>
-            <div>
-                <button
-                    className="edit-btn"
-                    aria-label="edit button"
-                    onClick={() => handleEdit(id)}
-                >
-                    <AiFillEdit />
-                </button>
-                <button
-                    className="clear-btn"
-                    aria-label="delete button"
-                    onClick={() => handleDelete(id)}
-                >
-                    <AiFillDelete />
-                </button>
-            </div>
-        </li>
-    )
+
+const ExpenseItem = ({expense}) => {
+const {id, charge, amount} = expense
+  return (
+    <li className="item">
+        <div className="info">
+            <span className="expense">{charge}</span>
+            <span className="amount">{amount}ETB</span>
+        </div>
+        <div>
+            <button
+                className="edit-btn"
+                aria-label="edit button"
+                //onClick={() => handleEdit(id)}
+            >
+            <AiFillEdit />
+            </button>
+            <button
+                className="clear-btn"
+                aria-label="delete button"
+                //onClick={() => handleDelete(id)}
+            >
+                <AiFillDelete />
+            </button>
+        </div>
+    </li>
+  )
 }
 
 export default ExpenseItem
